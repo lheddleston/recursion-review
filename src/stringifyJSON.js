d5edbae4a11375fb97483a9825b54c81da61168b
lheddleston <laura.heddleston@gmail.com>
2020-02-03 11:56:51 -0800

    Initial commit with stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  // stringify should take any type of object
+  // take object and return string
+  // ignore functions
+
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return '' + obj;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (typeof obj === 'function') { return '{}'; }
+  if (Array.isArray(obj)) {
+    var result = [];
+    if (obj.length === 0) { return '[]'; } else {
+      for (i = 0; i < obj.length; i++) {
+        result.push(stringifyJSON(obj[i]));
+      }
+      return '[' + result + ']';
+    }
+  }
+
+  if (typeof obj === 'object') {
+    if ((Object.keys(obj).length === 0)) { return '{}'; }
+    var result = [];
+    for (var key in obj) {
+      if (obj[key] === undefined) { return '{}'; }
+      if ( typeof obj[key] !== 'function' ) {
+        var value = stringifyJSON(obj[key]);
+        var stringKey = stringifyJSON(key);
+        result.push(stringKey + ':' + value);
+      }
+    }
+    return '{' + result + '}';
+  }
+
 };
