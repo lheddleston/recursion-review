396bea6fd7253693d5ac8f1f439fe97dc8c2f52b
lheddleston <laura.heddleston@gmail.com>
2020-02-03 20:29:56 -0800

    added logic for getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  //should return a result array of all elements that contains a specific 'className'
+  //takes in a className
+  //check if element contains that classname
+  //need to check if element children have classname as well
+  var result = [];
+
+  var classNameDetector = function(element) {
+
+    if (element.classList && element.classList.contains(className)) {
+      result.push(element);
+    }
+
+    if (element.childNodes !== undefined) {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        classNameDetector(element.childNodes[i]);
+      }
+    }
+  };
+  classNameDetector(document.body);
+  return result;
 };
